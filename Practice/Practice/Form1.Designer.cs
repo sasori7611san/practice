cd53772c0955e9317e8ddaf5e8bfd9f854c291e5
sasori7611san <107899079+sasori7611san@users.noreply.github.com>
2022-12-31 21:27:41 +0900

    make OneLineMemo

---
+++ Practice/Practice/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Practice
+{
+    partial class OneLineMemo
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Ok = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.End = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(367, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Ok
+            // 
+            this.Ok.Location = new System.Drawing.Point(41, 77);
+            this.Ok.Name = "Ok";
+            this.Ok.Size = new System.Drawing.Size(115, 56);
+            this.Ok.TabIndex = 1;
+            this.Ok.Text = "OK";
+            this.Ok.UseVisualStyleBackColor = true;
+            this.Ok.Click += new System.EventHandler(this.Ok_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(162, 77);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(115, 56);
+            this.Cancel.TabIndex = 2;
+            this.Cancel.Text = "キャンセル";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // End
+            // 
+            this.End.Location = new System.Drawing.Point(283, 77);
+            this.End.Name = "End";
+            this.End.Size = new System.Drawing.Size(115, 56);
+            this.End.TabIndex = 3;
+            this.End.Text = "End";
+            this.End.UseVisualStyleBackColor = true;
+            this.End.Click += new System.EventHandler(this.End_Click);
+            // 
+            // OneLineMemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 191);
+            this.Controls.Add(this.End);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Ok);
+            this.Controls.Add(this.textBox1);
+            this.Name = "OneLineMemo";
+            this.Text = "OneLineMemo";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Ok;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Button End;
+    }
+}
+
---
+++ Practice/Practice/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice
+{
+    public partial class OneLineMemo : Form
+    {
+        public OneLineMemo()
+        {
+            InitializeComponent();
+        }
+
+        private void Ok_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                DialogResult dialogResult = MessageBox.Show("保存しますか？", "ファイルに追加", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Title = "ファイルを保存する"; // ダイアログタイトル
+                    saveFileDialog.OverwritePrompt = true;       // 上書き時に警告する
+                    saveFileDialog.RestoreDirectory = true;      // 直前のフォルダを記憶する
+                    DialogResult result = saveFileDialog.ShowDialog();
+                    if (result == DialogResult.OK)
+                    {
+                        using (var sw = new System.IO.StreamWriter(saveFileDialog.FileName, true, Encoding.GetEncoding("UTF-8")))
+                        {
+                            sw.WriteLine(textBox1.Text);
+                        }
+                        MessageBox.Show("保存しました。");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("文字を入力してください。", "文字未入力", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                DialogResult dialogResult = MessageBox.Show("書き直しますか", "書き直し確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    textBox1.Text = string.Empty;
+                }
+            }
+            else
+            {
+                MessageBox.Show("入力がありません。", "文字未入力", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void End_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("終了しますか？", "終了確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
